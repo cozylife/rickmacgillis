5f8c136b6c087788ba8ee2d5092891903e25a3d2
Rick Mac Gillis <rick.macgillis@rickmacgillis.com>
2016-02-20 19:46:00 -0500

    Refactor
    Signed-off-by: Rick Mac Gillis <rick.macgillis@rickmacgillis.com>

---
+++ rick/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'dsn' => 'mysql:host=localhost;dbname=rickmacgillis',
+	'user' => 'root',
+	'password' => 'cozydev',
+	'prefix' => 'weh_',
+	'options' => [],
+];
---
+++ rick/config/email.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'driver'	=> 'PHPMailer',
+	'host'		=> 'mail.rickmacgillis.com',
+	'username'	=> 'contact@rickmacgillis.com',
+	'password'	=> 'Q_P8F+JGt&,BFl',
+	'port'		=> 465,
+	'securityProtocol'		=> 'tls',
+	'defaultToHtmlEmail'	=> false,
+];
